a59bedeaff57cb06ec962ec11226ffaca386092a
caprice04 <131392391+caprice04@users.noreply.github.com>
2023-08-04 22:14:43 +0530

    Add files via upload

---
+++ Day-5/Form Fields/main.js
@@ -0,0 +1,28 @@
+const formfield = document.getElementById('formfield');
+
+function add(){
+
+  let newField = document.createElement('input');
+
+  newField.setAttribute('type','text');
+  newField.setAttribute('name','text');
+  newField.setAttribute('class','text');
+  newField.setAttribute('siz',50);
+  
+  newField.setAttribute('placeholder','Optional Field');
+
+  formfield.appendChild(newField);
+
+}
+
+function remove(){
+
+  let input_tags = formfield.getElementsByTagName('input');
+
+  if(input_tags.length > 2) {
+
+    formfield.removeChild(input_tags[(input_tags.length) - 1]);
+
+
+  }
+}
