5c37da730cb74e854a75c0ac05b2d44b5460fbc4
Shubham Sharma <131392391+caprice04@users.noreply.github.com>
2023-08-26 20:40:52 +0530

    Add files via upload

---
+++ Day -27/Copy to Clipboard/main.js
@@ -0,0 +1,16 @@
+let copyText = document.getElementById("copy-text");
+
+copyText.querySelector("button").addEventListener("click", function () {
+
+	let input = copyText.querySelector("#myText");
+	input.select();
+
+	document.execCommand("copy");
+	copyText.classList.add("active");
+	window.getSelection().removeAllRanges();
+	
+});
+
+setTimeout(function () {
+    copyText.classList.remove("active");
+}, 1000);
